c1171b7189fe292d28865225fac366a85a4e6c1b
Giordisg <giordisg14@outlook.com>
2020-11-10 21:25:23 -0400

    Add project files.

---
+++ Conexion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ProyectoFinal
+{
+    class Conexion
+    {
+        public static SqlConnection GetSqlWinAuthenticationConnetion()
+        {
+            SqlConnection cn = new SqlConnection("Data Source=localhost\\SQLEXPRESS;Initial Catalog=PROYECTOFC#;Integrated Security=true;");
+            cn.Open();
+            return cn;
+        }
+    }
+}
